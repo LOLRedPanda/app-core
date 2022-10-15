2d43ac0e567885d3b7fff398b59160a53524a4fa
Nate White <natewhite1999@gmail.com>
2022-10-15 15:37:45 -0400

    list the top five most used champions

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
+const fs = require("fs")
 const axios = require('axios')
+const jsonQuery = require('json-query')
+const { lookup } = require('dns')
+const { json } = require('express')
 
 
 
-async function getSummonerId() {
+async function getTop5Champions() {
 
     const {status, data} = await axios.get(
         'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/RedPanda0129', 
@@ -18,21 +22,38 @@ async function getSummonerId() {
     
     console.log(id)
 
-    return id
+    getChampionMastery(id)
 
 }
 
-async function getChampionMastery() {
+async function getChampionMastery(id) {
     const {status, data} = await axios.get(
-        `https://na1.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${puuid}`, 
+        `https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}/top`, 
         {
             params: {
+                count: 5,
                 api_key: process.env.RIOT_API_KEY
             }
-    })
+    }
+    )
+    const jsonString = fs.readFileSync("./static-data/champions.json", "utf-8")
+    const rawData = JSON.parse(jsonString)
+    const championData = rawData.data
+    // console.log(championData)
+    const top5 = []
+    data.forEach(
+        (champion) => {
+            const {championId} = champion
+            const {name} = jsonQuery(`[**][key=${championId}]`, {data: championData}).value
+            top5.push(name)
+        }
+    )
+    console.log(top5)
+    
+  
 }
 
-getSummoner()
+getTop5Champions()
 
 
 

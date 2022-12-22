8abe92632264859c2491959e3c921500fccf8685
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2022-12-22 15:27:20 -0500

    working test suite for axios

--- api/spec/restclient.spec.js
+++ api/spec/restclient.spec.js
@@ -1,25 +1,33 @@
-// import axios from 'axios'
-// const { RestClient } = require("../src/services/restClient")
-// //mockaxios
+const axios = require('axios')
+const { RestClient } = require("../src/services/restClient")
+//mockaxios
 
-// describe('RestClient', () => {
-
+describe('RestClient', () => {
+//  const axiosSpy = createSpy()
+//  let client
 //     beforeAll(() => {
-//         match = new RestClient()
+//         client = new RestClient()
 //     })
 
-//     describe('get', () => {
-//         it('Should return a result on success', async () => {
-//           //have mock axois return 200
-//           //expect responce.data
-//         })
-//         it('if it is a 403 it should return an error', async () => {
-//           //have mock axois return 403 
-//           //expect function
-//         })
-//         it('if it is a 429 should retry and sleep', async () => {
-//           //have mock axois return 429 
-//           //expect error
-//         })
-//     })
-// })
+//     fdescribe('get', () => {
+        describe('get', () => {
+            it('Should return a result on success', async () => {
+                //spyOn(axios, 'get').and.resolveWith({ data:{status:200} })
+            })
+        })
+        //it('Should return a result on success', async () => {
+        //     spyOn(axios, 'get').andResolveTo({ data:{status:200} })
+        //    const result = client.get('fake url', {})
+        //   //expect responce.data.status
+        //   expect(result).toEqual({status:200})
+        //})
+        // it('if it is a 403 it should return an error', async () => {
+        //   //have mock axois return 403 
+        //   //expect function
+        // })
+        // it('if it is a 429 should retry and sleep', async () => {
+        //   //have mock axois return 429 
+        //   //expect error
+        // })
+    //})
+})

5ac55a9b1fe0998786c3b524689c8a4684139eb3
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-01-02 18:30:33 -0500

    added a new resource to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,3 +15,10 @@ resource "azurerm_resource_group" "lol-scout-resource-group" {
   name     = "dev-lol-scout-rg"
   location = "eastus"
 }
+
+resource "azurerm_storage_account" "lol-scout-storage-account" {
+  name                = "devlolscoutsa01"
+  resource_group_name = azurerm_resource_group.lol-scout-resource-group.name
+  location            = azurerm_resource_group.lol-scout-resource-group.location
+  account_tier        = "Standard"
+}

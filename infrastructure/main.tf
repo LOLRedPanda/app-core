f96edf6ad83007cb267a2cac96049615ba3933c1
NicoleSelig <nlselig@sep.com>
2023-01-02 18:39:04 -0500

    comment out the back end temporarily

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "dev-lol-scout-rg"
-    storage_account_name = "devlolscoutsa01"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "dev-lol-scout-rg"
+  #   storage_account_name = "devlolscoutsa01"
+  #   container_name       = "terraform-state"
+  #   key                  = "terraform.tfstate"
+  # }
 }
 
 resource "azurerm_resource_group" "lol-scout-resource-group" {

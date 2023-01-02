95ed16d612a5c22eab0acd5406a81b52d3c1a4ea
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-01-02 18:14:26 -0500

    added locals

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,16 +2,22 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  resource_group_name  = "${var.ENVIRONMENT}-lol-scout-rg"
+  storage_account_name = "${var.ENVIRONMENT}lolscoutsa01"
+  name                 = "${var.ENVIRONMENT}-lol-scout-rg"
+}
+
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${var.ENVIRONMENT}-lol-scout-rg"
-    storage_account_name = "${var.ENVIRONMENT}lolscoutsa01"
+    resource_group_name  = locals.resource_group_name
+    storage_account_name = locals.storage_account_name
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "lol-scout-resource-group" {
-  name     = "${var.ENVIRONMENT}-lol-scout-rg"
+  name     = locals.name
   location = "eastus"
 }

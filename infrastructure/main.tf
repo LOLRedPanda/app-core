22ae00d6f0d46f5e59515ccd94fc403b668ef448
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-01-02 17:39:16 -0500

    added terraform and azure cli

---
+++ infrastructure/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm"{
+    features{}
+}
+
+terraform{
+    backend "azurerm"{
+        resource_group_name = "dev-lol-scout-rg"
+        storage_account_name = "devlolscoutsa01"
+        container_name = "terraform-state"
+        key = "terraform.tfstate"
+    }
+}
+
+resource "azurerm_resource_group" "lol-scout-resource-group" {
+    name = "dev-lol-scout-rg"
+    location = "eastus"
+}

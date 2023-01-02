f086395f9858cd12545e7b33b2a6590ad536ef66
NicoleSelig <nlselig@sep.com>
2023-01-02 18:46:14 -0500

    add the storage container for terraform state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,15 +3,28 @@ provider "azurerm" {
 }
 
 terraform {
-  # backend "azurerm" {
-  #   resource_group_name  = "dev-lol-scout-rg"
-  #   storage_account_name = "devlolscoutsa01"
-  #   container_name       = "terraform-state"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "dev-lol-scout-rg"
+    storage_account_name = "devlolscoutsa01"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
 }
 
-resource "azurerm_resource_group" "lol-scout-resource-group" {
+resource "azurerm_resource_group" "lol_scout_rg" {
   name     = "dev-lol-scout-rg"
   location = "eastus"
+}
+
+resource "azurerm_storage_account" "lol_scout_sa" {
+  name                     = "devlolscoutsa01"
+  resource_group_name      = azurerm_resource_group.lol_scout_rg.name
+  location                 = azurerm_resource_group.lol_scout_rg.location
+  account_tier             = "Standard"
+}
+
+resource "azurerm_storage_container" "lol_scout_sa_container" {
+  name                  = "terraform-state"
+  storage_account_name  = azurerm_storage_account.lol_scout_sa.name
+  container_access_type = "private"
 }

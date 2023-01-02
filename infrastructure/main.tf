e7079952c7f1da332b18cbb9c0e378cbbe815b7c
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-01-02 18:26:12 -0500

    reverted it back to hard code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,22 +2,16 @@ provider "azurerm" {
   features {}
 }
 
-locals {
-  resource_group_name  = "${var.ENVIRONMENT}-lol-scout-rg"
-  storage_account_name = "${var.ENVIRONMENT}lolscoutsa01"
-  name                 = "${var.ENVIRONMENT}-lol-scout-rg"
-}
-
 terraform {
   backend "azurerm" {
-    resource_group_name  = locals.resource_group_name
-    storage_account_name = locals.storage_account_name
+    resource_group_name  = "dev-lol-scout-rg"
+    storage_account_name = "devlolscoutsa01"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "lol-scout-resource-group" {
-  name     = locals.name
+  name     = "dev-lol-scout-rg"
   location = "eastus"
 }
--- infrastructure/varibles.tf
+++
@@ -1,3 +0,0 @@
-variable "ENVIRONMENT" {
-  type = string
-}

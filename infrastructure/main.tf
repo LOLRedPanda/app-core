7d846e67cd05eacd358c1f99deaaaaf9d2914608
NicoleSelig <nlselig@sep.com>
2023-01-02 19:35:57 -0500

    import resources into terraform state

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,9 +21,10 @@ resource "azurerm_storage_account" "lol_scout_sa" {
   resource_group_name      = azurerm_resource_group.lol_scout_rg.name
   location                 = azurerm_resource_group.lol_scout_rg.location
   account_tier             = "Standard"
+  account_replication_type = "GRS"
 }
 
-resource "azurerm_storage_container" "lol_scout_sa_container" {
+resource "azurerm_storage_container" "terraform_state_sa" {
   name                  = "terraform-state"
   storage_account_name  = azurerm_storage_account.lol_scout_sa.name
   container_access_type = "private"

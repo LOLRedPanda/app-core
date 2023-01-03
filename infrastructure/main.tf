a628c1ce53aad9e7edb8f504209cfc1fc2d59396
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-01-03 12:25:31 -0500

    added back the service plan free version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,3 +15,14 @@ resource "azurerm_resource_group" "lol_scout_rg" {
   name     = "dev-lol-scout-rg"
   location = "eastus"
 }
+
+resource "azurerm_app_service_plan" "app_sp" {
+  name                = "devlolscoutsp01"
+  location            = azurerm_resource_group.app_sp.location
+  resource_group_name = azurerm_resource_group.app_sp.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}

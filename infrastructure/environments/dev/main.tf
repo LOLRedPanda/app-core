fc664d2b3c5ab44e0506d66d73cfce118b8af3b0
NicoleSelig <nlselig@sep.com>
2023-01-03 18:14:14 -0500

    explicitly call the tf file

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "main" {
-    source = "../.."
+    source = "../../main.tf"
     resource_group_name = locals.resource_group_name
     storage_account_name = locals.storage_account_name
     app_service_plan_name = locals.app_service_plan_name
---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "app_service_plan_name" {
+    type = string
+    description = "The name of the app service plan"
+}
+
+variable "resource_group_name" {
+    type = string
+    description= "The name of the resource group."
+}
+
+variable "location" {
+    type = string
+    description = "The location the resources are associated with."
+}

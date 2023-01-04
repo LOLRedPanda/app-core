b96f85309ab84e7ec78cbaf5f2d98336a55f7962
NicoleSelig <nlselig@sep.com>
2023-01-04 07:23:58 -0500

    try creating a tfvars file in github

---
+++ infrastructure/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "${var.environment}-lol-scout-rg"
+    storage_account_name = "devlolscoutsa01"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "lol_scout_rg" {
+  name     = "dev-lol-scout-rg"
+  location = "eastus"
+}
+
+resource "azurerm_app_service_plan" "app_sp" {
+  name                = "devlolscoutsp01"
+  location            = azurerm_resource_group.lol_scout_rg.location
+  resource_group_name = azurerm_resource_group.lol_scout_rg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,4 @@
+variable "environment" {
+  type = string
+  description = "The environment where resources will be built."
+}

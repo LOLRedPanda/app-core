bf74db686fbf235febc4d348bba8607657aac28e
NicoleSelig <nlselig@sep.com>
2023-01-10 06:45:14 -0500

    remove node version

--- infrastructure/modules/linux_web_app/main.tf
+++ infrastructure/modules/linux_web_app/main.tf
@@ -3,7 +3,6 @@ resource "azurerm_linux_web_app" "app_api" {
   resource_group_name = var.resource_group_name
   location            = var.location
   service_plan_id     = var.service_plan_id
-  node_version = "18-lts"
 
   site_config {
     always_on = false

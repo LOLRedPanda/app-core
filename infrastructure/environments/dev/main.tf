5254baf059425d99a2ecf4ae597a52588f8ed832
NicoleSelig <nlselig@sep.com>
2023-01-10 12:09:48 -0500

    fix tab

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -25,6 +25,7 @@ module "infra" {
   web_app_name = "${local.env}lolscoutwa01"
   location = local.location
   app_env_vars = {
+    WEBSITE_WEBDEPLOY_USE_SCM = true
     RIOT_API_KEY = local.riot_api_key
   }
 }

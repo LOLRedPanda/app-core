a93a4f47390e566696366827ed21908cc9bd874d
NicoleSelig <nlselig@sep.com>
2023-01-10 13:29:39 -0500

    passing ids in as vars

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -5,6 +5,10 @@ locals {
 }
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
 
 terraform {
--- infrastructure/environments/dev/variables.tf
+++ infrastructure/environments/dev/variables.tf
@@ -5,3 +5,20 @@ variable "env" {
 variable "riot_api_key" {
   description = "The Riot Api Key"
 }
+
+variable "subscription_id" {
+  description = "Subscription Id of the service principal"
+}
+
+variable "client_id" {
+  description = "client Id of the service principal"
+}
+
+variable "tenant_id" {
+  description = "tenant Id of the service principal"
+}
+
+variable "client_secret" {
+  description = "client Id of the service principal"
+}
+

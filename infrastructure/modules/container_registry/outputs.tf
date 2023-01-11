0ca1ac08135d1630a350a002ffb07dbe6503823c
NicoleSelig <nlselig@sep.com>
2023-01-11 08:27:03 -0500

    remove the managed identities

--- infrastructure/modules/container_registry/outputs.tf
+++ infrastructure/modules/container_registry/outputs.tf
@@ -1,11 +1,3 @@
 output "login_server" {
     value = azurerm_container_registry.acr.login_server
 }
-
-output "principal_id" {
-    value = azurerm_container_registry.acr.identity.0.principal_id
-}
-
-output "tenant_id" {
-    value = azurerm_container_registry.acr.identity.0.tenant_id
-}

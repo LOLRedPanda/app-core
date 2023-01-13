020fb24181dab6d280e3cf224a243a661f91a547
NicoleSelig <nlselig@sep.com>
2023-01-13 04:57:39 -0500

    switch to basic

--- infrastructure/modules/service_plan/main.tf
+++ infrastructure/modules/service_plan/main.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "app_sp" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }

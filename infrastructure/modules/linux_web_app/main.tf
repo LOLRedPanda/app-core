45cfc9356d18a0f717885b3c24e0099c503dae24
NicoleSelig <nlselig@sep.com>
2023-01-28 16:48:35 -0500

    up the time limit and specify the port

--- infrastructure/modules/linux_web_app/main.tf
+++ infrastructure/modules/linux_web_app/main.tf
@@ -20,8 +20,9 @@ resource "azurerm_linux_web_app" "app_api" {
     var.app_env_vars,
     {
       DOCKER_ENABLE_CI = true
+      WEBSITES_PORT=3000
       DOCKER_REGISTRY_SERVER_URL = "https://${var.registry_server_url}"
-      WEBSITES_CONTAINER_START_TIME_LIMIT = 1800
+      WEBSITES_CONTAINER_START_TIME_LIMIT = 3200
     }
   )
 }

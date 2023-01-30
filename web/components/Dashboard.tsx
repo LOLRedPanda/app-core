c0b8d589453744c23bd8b35ad401947be2e10bda
NicoleSelig <nlselig@sep.com>
2023-01-29 20:23:23 -0500

    uncomment members

--- web/components/Dashboard.tsx
+++ web/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 import PropTypes, {InferProps} from 'prop-types'
 import TopCards from './TopCards';
 import BarChart from './BarChart';
-// import Members from './Members';
+import Members from './Members';
 import ListBox from './ListBox';
 
 function DashBoard({data}: InferProps<typeof DashBoard.propTypes>) {
@@ -11,7 +11,7 @@ function DashBoard({data}: InferProps<typeof DashBoard.propTypes>) {
       <TopCards teamData={data[0]} />
       <div className="pt-4 pr-4 grid md:grid-cols-3 grid-cols-1 gap-4 ">
         <BarChart teamData={data[0]} />
-        {/* <Members teamData={data[0]} /> */}
+        <Members teamData={data[0]} />
       </div>
     </div>
   );

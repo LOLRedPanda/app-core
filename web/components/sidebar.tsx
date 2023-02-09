bda94493950a51825daff63c1fcc650363e3b8cf
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-02-09 03:51:08 -0500

    made the Team.tsx ui match up with the stats UI and changed icons to more resemble what the page is

--- web/components/sidebar.tsx
+++ web/components/sidebar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes, {InferProps} from 'prop-types'
 import Image from 'next/image'
 import Link from 'next/link'
-import {RxDashboard} from 'react-icons/rx'
-import {IoMdPeople} from 'react-icons/io'
+import {BiStats} from 'react-icons/bi'
+import {BsTrophy} from 'react-icons/bs'
 import {FiSettings} from 'react-icons/fi'
 import RedPandaLogo from '../public/assets/redPandaLogos/circle_panda.png'
 
@@ -18,17 +18,12 @@ function sidebar({children}:InferProps<typeof sidebar.propTypes>) {
                 <span className='border-b-[1px] border-gray-200 w-full p-2'></span>
                 <Link href='/'>
                     <div className='bg-[#101021] hover:bg-gray-200 shadow-md shadow-[#FF514D] cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                        <RxDashboard size={24} color="#ED009C"/>
+                        <BiStats size={24} color="#ED009C"/>
                     </div>
                 </Link>
                 <Link href='/teams'>
                     <div className='bg-[#101021] hover:bg-gray-200 shadow-md shadow-[#FF514D] cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                        <IoMdPeople size={24} color="#ED009C"/>
-                    </div>
-                </Link>
-                <Link href='/'>
-                    <div className='bgbg-[#101021] hover:bg-gray-200 shadow-md shadow-[#FF514D] cursor-pointer my-4 p-3 rounded-lg inline-block'>
-                        <FiSettings size={24} color="#ED009C"/>
+                        <BsTrophy size={24} color="#ED009C"/>
                     </div>
                 </Link>
             </div>
--- web/pages/teams.tsx
+++ web/pages/teams.tsx
@@ -23,14 +23,13 @@ function teams() {
         return 0
     });
   return (
-    <div className='bg-red-300 min-h-screen'>
+    <div className='bg-[#101021]'>
         <div className = "flex justify-between pr-4 pt-4">   
         <Image src={PxeLogo} alt='No Image' width={200} height={100}/>
-        <h2 className='font-bold text-[50px] pt-14'>Pheonix Esports League</h2>
-        <h2>Welcome Back, Chris</h2>
+        <h2 className='text-[#FF514D] font-bold text-[50px] pt-14'>Pheonix Esports League</h2>
     </div>
         <div className='p-4'>
-            <div className='w-full m-auto p-4 border rounded-lg bg-red-100 overflow-y-auto'>
+            <div className='w-full m-auto p-4 border rounded-lg bg-[#da2b82] overflow-y-auto'>
                 <div className='my-3 p-2 grid md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-center justify-between cursor-pointer'>
                     <span className='hidden sm:grid'>Ranked</span>
                     <span>Logo</span>
@@ -40,17 +39,17 @@ function teams() {
                 </div>
                 <ul>
                     {data.map((teamData, id) => (
-                        <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-center justify-between cursor-pointer'>
-                            <p className='text-Black sm:text-left text-center text-[40px]'>{teamData.ranked}</p>
+                        <li key={id} className='bg-[#101021] rounded-lg my-3 p-2 grid md:grid-cols-5 sm:grid-cols-4 grid-cols-3 items-center justify-between cursor-pointer'>
+                            <p className='text-[#FF514D] sm:text-left text-center text-[40px]'>{teamData.ranked}</p>
                             <div className='flex items-center'>
-                                <div className='bg-red-100 p-1 rounded-lg'>
+                                <div className='bg-[#8d3f3f] p-1 shadow-md shadow-[#573d3d] rounded-lg'>
                                 <Image src={teamData.logo} alt={''} width={100} height={100} />
                                 </div>
                             </div>
-                            <p className='text-Black sm:text-left text-center text-[45px]'>{teamData.name}</p>
-                            <p className='hidden md:flex text-[45px]'>{teamData.wins + '-' + teamData.loses}</p>
-                            <div className='sm:flex hidden justify-between items-center'>
-                                <p className='hidden md:flex text-[45px]'>{teamData.captain}</p>
+                            <p className='text-[#FF514D] sm:text-left text-center text-[45px]'>{teamData.name}</p>
+                            <p className='text-[#FF514D] hidden md:flex text-[45px]'>{teamData.wins + '-' + teamData.loses}</p>
+                            <div className='text-[#FF514D] sm:flex hidden justify-between items-center'>
+                                <p className=' hidden md:flex text-[45px]'>{teamData.captain}</p>
                             </div>
                         </li>
                     ))}

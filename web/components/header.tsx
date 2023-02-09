5450fb787d52ab44eaff945c6423998068068dee
Nate White <103273469+Nathanlee29@users.noreply.github.com>
2023-02-09 04:03:09 -0500

    added a new header and its somehow 4am and I didnt notice

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function header() {
   return (
-    <div>
-      <div className = "flex pr-4 pt-4">   
-          <h2 className='text-[#7D98A1]'>Welcome Back, Chris</h2>
+    <div className = 'border-b-4 border-[#FF514D]'>
+      <div className = "flex pr-4 pt-4  ">   
+          <h2 className='text-[#FF514D] font-bold text-[30px]'>Your Friendly Red Panda Stat Tracker</h2>
       </div>
     </div>
   )

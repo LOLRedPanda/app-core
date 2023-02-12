587b956d19083604eb17bd762bea275b4a803021
Nicole Selig <nicoleleeselig@gmail.com>
2023-02-12 10:42:55 -0500

    blur position holder

--- web/components/MemberItem.tsx
+++ web/components/MemberItem.tsx
@@ -5,7 +5,7 @@ import PropTypes, {InferProps} from 'prop-types'
 function MemberItem({image, name, position}: InferProps<typeof MemberItem.propTypes>) {
     return (<li className='bg-[#101021] shadow-md border border-[#ff514d] shadow-[#ff514d] rounded-lg my-3 p-2 flex items-center'>
         <div >
-            <Image src={image} alt='/' width={45} height={100} />
+            <Image src={image} alt='/' placeholder='blur' width={45} height={100} />
         </div>
         <div className='pl-4'>
             <p className='text-[#EAD5E6] font-bold'>{name}</p>

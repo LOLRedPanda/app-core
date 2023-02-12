e3ea33e870bd90acc5832c5ccfa535600574a7d3
Nicole Selig <nicoleleeselig@gmail.com>
2023-02-12 09:21:07 -0500

    simplify source path

--- web/components/Members.tsx
+++ web/components/Members.tsx
@@ -4,7 +4,7 @@ import PropTypes, {InferProps} from 'prop-types'
 
 function Members({teamData} : InferProps<typeof Members.propTypes>) {
   const {members} = teamData
-  const memberItems = members.map((member: any) => <Member key={member.name} image={`/../public/assets/RoleLogos/${member.position.toLowerCase()}.png`} name={member.name} position={member.position} />)
+  const memberItems = members.map((member: any) => <Member key={member.name} image={`/assets/RoleLogos/${member.position.toLowerCase()}.png`} name={member.name} position={member.position} />)
   
   return (
     <div className='w-full overflow-auto col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border border-[#28bc0c] shadow-lg shadow-[#28bc0c] rounded-lg bg-[#101021]'>
--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['localhost', 'devlolscoutwa02.azurewebsites.net'],
-  },
 }

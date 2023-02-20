7849f2b77f3c7183451a9d3a8c7a09004e195c2a
Nicole Selig <nicoleleeselig@gmail.com>
2023-02-20 13:29:30 -0500

    pulled data from google sheet succesfully

--- web/data/team-generator.js
+++ web/data/team-generator.js
@@ -1,6 +1,59 @@
 const data = require('./teams.json')
+require('dotenv').config()
+const { GoogleSpreadsheet } = require('google-spreadsheet')
 
-const newTeams = data.map((team) => {
+const doc = new GoogleSpreadsheet(
+	'1AcxvdJG0Yf9nDe-MuYBtnOvAPUf04T0PwZKWxPXs45w'
+)
+async function auth() {
+	await doc.useServiceAccountAuth({
+		client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+		private_key: process.env.GOOGLE_PRIVATE_KEY,
+	})
+
+	await doc.loadInfo()
+
+	const sheet = doc.sheetsByIndex[0]
+
+	const rows = await sheet.getRows()
+	const spreadsheetData = rows.map((row) => {
+		const {
+			playerId,
+			teamId,
+			teamName,
+			playerName,
+			position,
+			captain,
+			totalKills,
+			totalDeaths,
+			totalAssists,
+			totalKDA,
+			totalDMGPM,
+			totalCSPM,
+			totalMins,
+		} = row
+
+		return {
+			playerId,
+			teamId,
+			teamName,
+			playerName,
+			position,
+			captain,
+			totalKills,
+			totalDeaths,
+			totalAssists,
+			totalKDA,
+			totalDMGPM,
+			totalCSPM,
+			totalMins,
+		}
+	})
+}
+
+auth()
+
+const newTeams = spreadsheetData.map((team) => {
 	const { members } = team
 	const { KDA, CSPM, DMGPM } = team.stats
 
@@ -26,4 +79,4 @@ const newTeams = data.map((team) => {
 	}
 })
 
-console.log(JSON.stringify(newTeams, null, 2))
+// console.log(JSON.stringify(newTeams, null, 2))
